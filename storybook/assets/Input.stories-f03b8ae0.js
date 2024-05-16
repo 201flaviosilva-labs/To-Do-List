aab4633529ec6297f4fa3a1a26383143d2b119e3
201flaviosilva <55815265+201flaviosilva@users.noreply.github.com>
2024-05-16 10:58:34 +0000

    Deploying to gh-pages from @ 201flaviosilva-labs/To-Do-List@d5a6554951de01d6b168237b44d11398d8a72557 🚀

---
+++ storybook/assets/Input.stories-f03b8ae0.js
@@ -0,0 +1,9 @@
+import{a as m}from"./jsx-runtime-9a982444.js";import{r as c}from"./index-928ed2fb.js";import{I as o}from"./Input-819800f2.js";import"./_commonjsHelpers-725317a4.js";const f={title:"Pages / Account / Input",component:o},e=n=>{const[t,p]=c.useState({name:"",username:"",password:""});return console.info(t),m(o,{label:"Name",property:"name",state:t,setState:p,...n})};e.__docgenInfo={description:"",methods:[],displayName:"Default"};var a,s,r;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`args => {
+  const [state, setState] = useState({
+    name: "",
+    username: "",
+    password: ""
+  });
+  console.info(state);
+  return <Input label="Name" property="name" state={state} setState={setState} {...args} />;
+}`,...(r=(s=e.parameters)==null?void 0:s.docs)==null?void 0:r.source}}};const S=["Default"];export{e as Default,S as __namedExportsOrder,f as default};
---
+++ storybook/assets/withPadding-c63bae73.js
@@ -0,0 +1,3 @@
+import{a as i}from"./jsx-runtime-9a982444.js";import{s as d}from"./styled-components.browser.esm-6e0b86f1.js";const o=d.div`
+    margin: 16px;
+`,s=r=>i(o,{children:i(r,{})});s.__docgenInfo={description:"",methods:[],displayName:"withPadding"};export{s as w};
